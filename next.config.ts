239d9adb2da25caa543b02bed0a24d7c47559e9d
lasfloresdepluton <lasfloresdepluton@gmail.com>
2026-03-19 11:41:40 -0300

    Simplified next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,22 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // Empty turbopack config silences the webpack/turbopack mismatch warning
-  turbopack: {},
-  webpack: (config, { dev }) => {
-    if (dev) {
-      config.watchOptions = {
-        poll: 1000,
-        aggregateTimeout: 300,
-        ignored: [
-          '**/node_modules/**',
-          '**/.next/**',
-          '**/.git/**',
-        ],
-      };
-    }
-    return config;
-  },
   images: {
     remotePatterns: [
       {
